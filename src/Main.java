6a392edd3d59c4e5fe6ce0bf8f8f021ec2d3f8c8
nikbatura <nikbatura@mail.ru>
2023-05-15 03:55:54 +0700

    Проверьте дз

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void printSeparator() {
+        System.out.println("====================");
+    }
+
+    public static void main(String[] args) {
+
+
+        Author Bulgakov = new Author("Михаил","Булгаков");
+        printSeparator();
+        System.out.println("Автор - " + Bulgakov.getName() + " " + Bulgakov.getSurname());
+        Book dogHeart = new Book("Собачье сердце", Bulgakov, 1925);
+        System.out.println("Название книги - " + dogHeart.getName());
+        System.out.println("Год издания - " + dogHeart.getYear());
+        printSeparator();
+        Author Gogol = new Author("Николай","Гоголь");
+        System.out.println("Автор - " + Gogol.getName() + " " + Gogol.getSurname());
+        Book Viy = new Book("Вий",Gogol,1834);
+        System.out.println("Название книги - " + Viy.getName());
+        System.out.println("Год издания - " + Viy.getYear());
+        Viy.setYear(1835);
+        System.out.println("Год издания - " + Viy.getYear());
+        printSeparator();
+
+
+    }
+
+
+    }
